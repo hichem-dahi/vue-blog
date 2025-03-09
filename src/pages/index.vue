089fed98b03d6f2101e878f047b6d399934a9a02
Hichem <hichemdahi57@gmail.com>
2025-03-09 01:33:29 +0100

    Revert "feat: add list posts feature"
    This reverts commit 75d5c80c03d7eaf096737f9af25f84009707e46d.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,7 @@
 <template>
-  <router-view />
+  <HelloWorld />
 </template>
 
 <script lang="ts" setup>
-const router = useRouter()
-const route = useRoute()
-
-onMounted(() => {
-  if(route.path == '/')
-    router.push({path: '/home'})
-})
+  //
 </script>
