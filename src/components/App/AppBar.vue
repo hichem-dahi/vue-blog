a734ace6f8ce2f9d0eaa5062c427e466d6c6454b
Hichem <hichemdahi57@gmail.com>
2025-03-09 01:38:59 +0100

    feat: add AppBar to default layout

---
+++ src/components/App/AppBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar>
+    <template #prepend>
+      <div class="d-flex align-end ga-2 pa-4">
+        <div
+          class="text-h6 mx-4"
+          @click="router.push('/home')"
+        >
+          VueBlog
+        </div> 
+        <v-btn
+          variant="plain"
+          size="small"
+          text="My posts"
+          :to="{ path: '/my-posts' }"
+        />
+        <v-btn
+          variant="plain"
+          size="small"
+          text="New post"
+          :to="{ path: '/new-post' }"
+        />
+      </div>
+    </template>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+const router = useRouter()
+</script>
