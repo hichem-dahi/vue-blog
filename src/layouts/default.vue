ffbbdc6cd7eea32941db34ec26fb3d0c4d4de5fb
Hichem <hichemdahi57@gmail.com>
2025-03-09 01:37:54 +0100

    feat: add back button to default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,20 @@
 <template>
+  <AppBar />
   <v-main>
+    <v-btn
+      v-if="$route.path !== '/home'"
+      class="mx-12 mt-8 mb-4"
+      variant="plain"
+      size="x-small"
+      prepend-icon="mdi-arrow-left"
+      @click="$router.go(-1)"
+    >
+      Back
+    </v-btn>
     <router-view />
   </v-main>
-
-  <AppFooter />
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
